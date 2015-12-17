762ad1ce2c1c5d83c6a44bcf6e8a5bc8783551d9
peteflorence <pete.florence@gmail.com>
2015-12-17 12:52:43 -0500

    added all data in, finished parser

--- fla/mission_parser.py
+++ fla/mission_parser.py
@@ -1,43 +1,46 @@
+__author__ = 'peteflorence'
+
 import xml.etree.ElementTree as ET
 
-tree = ET.parse('indoor-mission/mission.xml')
+
+missionXMLFile = 'indoor-mission/mission.xml'
+
+
+tree = ET.parse(missionXMLFile)
 root = tree.getroot()
 
 # missionInfo is where START and GOAL locations are stored
 missionInfo = root.find('missionInfo')
 
+# Helper function that sets coordinate to 0 if not specified
+def readCoordinate(cartesianLocation, coordinate):
+  if cartesianLocation.attrib.get(coordinate) is None:
+    return 0
+  else:
+    return cartesianLocation.attrib[coordinate]
 
 # START location, parsed in Cartesian coordinates
 start = missionInfo.find('start')
-# for child in start:
-#   print child.tag, child.attrib
-
 startLocation = start.find('location')
-# for child in startLocation:
-#   print child.tag, child.attrib
-
 cartesianStartLocation = startLocation.find('cartesianLocation')
-xStartMeter = cartesianStartLocation.attrib['xMeter']
-yStartMeter = cartesianStartLocation.attrib['yMeter']
-zStartMeter = cartesianStartLocation.attrib['zMeter']
+
+xStartMeter = readCoordinate(cartesianStartLocation, 'xMeter')
+yStartMeter = readCoordinate(cartesianStartLocation, 'yMeter')
+zStartMeter = readCoordinate(cartesianStartLocation, 'ZMeter')
 
 print xStartMeter, yStartMeter, zStartMeter
 
 
 # GOAL location, parsed in Cartesian coordinates
 goal = missionInfo.find('goal')
-# for child in goal:
-#   print goal.tag, goal.attrib
-
 goalLocationList = goal.findall('location')
 for child in goalLocationList:
   if child.attrib['locationType'] == 'cartesian':
-    cartesianGoalLocation = child
+    goalLocation = child
+cartesianGoalLocation = goalLocation.find('cartesianLocation')
 
+xGoalMeter = readCoordinate(cartesianGoalLocation, 'xMeter')
+yGoalMeter = readCoordinate(cartesianGoalLocation, 'yMeter')
+zGoalMeter = readCoordinate(cartesianGoalLocation, 'zMeter')
 
-cartesianGoalLocation = cartesianGoalLocation.find('cartesianLocation')
-xGoalMeter = cartesianGoalLocation.attrib['xMeter']
-yGoalMeter = cartesianGoalLocation.attrib['yMeter']
-#zGoalMeter = cartesianGoalLocation.attrib['zMeter']
-
-print xGoalMeter, yGoalMeter
+print xGoalMeter, yGoalMeter, zGoalMeter
